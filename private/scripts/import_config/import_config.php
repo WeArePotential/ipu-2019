e2ad813dcf2601898ed93a201d388e626e418176
Peter Mason <pmasonids@gmail.com>
2019-03-12 16:57:09 +0000

    Implement CodePositive workflow management to clear cache, enable and disable modules etc.
    See http://www.codepositive.com/articles/automate-drupal-8-deployment-pantheon-quicksilver-scripts

---
+++ private/scripts/import_config/import_config.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Configure modules.
+ */
+
+$config_directory = '';
+
+if (defined('PANTHEON_ENVIRONMENT')) {
+
+  switch(PANTHEON_ENVIRONMENT) {
+    case 'live':
+    case 'test':
+    case 'dev':
+      $config_directory = dirname(__FILE__) . '/config/' . PANTHEON_ENVIRONMENT;
+      break;
+    default:
+      //This will be multidev instances - use dev settings
+      $config_file = dirname(__FILE__) . '/config/multidev/config.inc';
+      break;
+  }
+}
+
+if (!empty($config_directory)) {
+  passthru("drush cim --partial --source=$config_directory --yes");
+}
+
+// Confirmation for Terminus.
+echo('Configuration imported.' . "\n");
